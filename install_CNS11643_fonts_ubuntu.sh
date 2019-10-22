88bb7586106578b15691c03070be9185b6396649
hms5232 <43672033+hms5232@users.noreply.github.com>
2019-10-22 15:23:32 +0800

    小修一些地方
    增加聯絡資訊
    註解微調
    建立~/.font輸出資訊後會換行
    正楷體、正宋體英文
    更新字型指令參數部份
    完成後的輸出取消提示音

--- install_CNS11643_fonts_ubuntu.sh
+++ install_CNS11643_fonts_ubuntu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# ######################################################################################################## #
 # Author: hms5232
-# source code and manual on https://github.com/hms5232/ubunut-install-CNS11643-fonts
+# Source code and manual on https://github.com/hms5232/ubunut-install-CNS11643-fonts
+# Contact, report bugs or ask questions: https://github.com/hms5232/ubunut-install-CNS11643-fonts/issues
+# ######################################################################################################## #
 
-# if $HOME/.fonts dir not exist, then create it.
+# if ~/.fonts dir not exist, then create it.
 if [ ! -d "$HOME/.fonts" ] ; then
     mkdir $HOME/.fonts
-    echo "create ~/.fonts/"
+    echo -e "create ~/.fonts/ \n"
 fi
 
 # donwload CNS11643中文標準交換碼全字庫（簡稱全字庫）
 # 正楷體、正宋體
+# TW-Kai, TW-Sung
 wget http://www.cns11643.gov.tw/AIDB/Open_Data.zip
 
 # unzip
@@ -20,12 +24,12 @@ unzip Open_Data.zip
 cp -i Open_Data/Fonts/*.ttf $HOME/.fonts
 
 # refresh fonts cache
-sudo fc-cache -f -v
+sudo fc-cache -fv
 
 # remove zip and unzip dir
 rm Open_Data.zip
 rm -r Open_Data/
 
 # finally
-echo -e "\n\a"
+echo -e "\n\n"
 echo -e "============== This is the end of script. ============== \n"
