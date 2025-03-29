5d9e44ce0ba9cd4fa2ecfce5e0dd5c7b7b5bead0
hms5232 <hms5232@hhming.moe>
2025-03-29 19:21:04 +0800

    fix: broken download link
    fix #2

--- install_CNS11643_fonts_ubuntu.sh
+++ install_CNS11643_fonts_ubuntu.sh
@@ -15,6 +15,10 @@ if [ ! -d "$HOME/.fonts" ] ; then
     echo -e "create ~/.fonts/ \n"
 fi
 
+# ---------------------------
+# 原始打包整合的單一壓縮檔
+# ---------------------------
+
 # when Open_Data.zip exists, users must check out they wanna to download again or not
 if [ -f "Open_Data.zip" ] ; then
 	read -p "Open_Data.zip already exists. Do you want to delete it and download again? [y/n]" re_download
@@ -26,6 +30,45 @@ if [ -f "Open_Data.zip" ] ; then
 			;;
 		*)
 			read -p "Use the existing Open_Data.zip? [y/n]" re_use
+			case $re_use in
+				[yY][eE][sS]|[yY])
+					need_download=false
+					echo -e "Skip download."
+					echo -e "\n"
+                    # let's hash it~
+                    # but we don't have offical sha1sum file Orz
+                    hash=$(sha1sum Open_Data.zip | cut -d ' ' -f 1)
+                    echo -e "\n The SHA1 value of downloaded file is \n"
+                    echo -e "\t>>>>> $hash <<<<<\n"
+
+                    # unzip
+                    unzip Open_Data.zip
+					;;
+				*)
+					echo -e "\n"
+					echo -e "============== Exit shell script! =============="
+					exit
+					;;
+			esac
+	esac
+fi
+
+# ---------------------------
+# 單一正宋體壓縮檔
+# ---------------------------
+
+# when Fonts_Sung.zip exists, users must check out they wanna to download again or not
+if [ -f "Fonts_Sung.zip" ] ; then
+	read -p "Fonts_Sung.zip already exists. Do you want to delete it and download again? [y/n]" re_download
+	case $re_download in
+		[yY][eE][sS]|[yY])
+			echo -e "remove Fonts_Sung.zip...\n"
+			rm Fonts_Sung.zip
+			echo -e "\n"
+            need_download=true
+			;;
+		*)
+			read -p "Use the existing Fonts_Sung.zip? [y/n]" re_use
 			case $re_use in
 				[yY][eE][sS]|[yY])
 					need_download=false
@@ -43,20 +86,84 @@ fi
 
 # Let's download font
 if $need_download ; then
+    if [ ! -d "Open_Data/" ] ; then
+        mkdir Open_Data
+    fi
+
 	# download CNS11643中文標準交換碼全字庫（簡稱全字庫）
 	# 正楷體、正宋體
 	# TW-Kai, TW-Sung
-	wget http://www.cns11643.gov.tw/AIDB/Open_Data.zip
+	wget https://www.cns11643.gov.tw/opendata/Fonts_Sung.zip
 fi
 
-# let's hash it~
-# but we don't have offical sha1sum file Orz
-hash=$(sha1sum Open_Data.zip | cut -d ' ' -f 1)
-echo -e "\n The SHA1 value of downloaded file is \n"
-echo -e "\t>>>>> $hash <<<<<\n"
+if [ -f "Fonts_Sung.zip" ] ; then
+    # let's hash it~
+    # but we don't have offical sha1sum file Orz
+    hash=$(sha1sum Fonts_Sung.zip | cut -d ' ' -f 1)
+    echo -e "\n The SHA1 value of Fonts_Sung.zip is \n"
+    echo -e "\t>>>>> $hash <<<<<\n"
 
-# unzip
-unzip Open_Data.zip
+    # unzip
+    unzip Fonts_Sung.zip -d Open_Data
+fi
+
+# ---------------------------
+# 單一正楷體壓縮檔
+# ---------------------------
+
+# when Fonts_Kai.zip exists, users must check out they wanna to download again or not
+if [ -f "Fonts_Kai.zip" ] ; then
+	read -p "Fonts_Kai.zip already exists. Do you want to delete it and download again? [y/n]" re_download
+	case $re_download in
+		[yY][eE][sS]|[yY])
+			echo -e "remove Fonts_Kai.zip...\n"
+			rm Fonts_Kai.zip
+			echo -e "\n"
+            need_download=true
+			;;
+		*)
+			read -p "Use the existing Fonts_Kai.zip? [y/n]" re_use
+			case $re_use in
+				[yY][eE][sS]|[yY])
+					need_download=false
+					echo -e "Skip download."
+					echo -e "\n"
+					;;
+				*)
+					echo -e "\n"
+					echo -e "============== Exit shell script! =============="
+					exit
+					;;
+			esac
+	esac
+fi
+
+# Let's download font
+if $need_download ; then
+    if [ ! -d "Open_Data/" ] ; then
+        mkdir Open_Data
+    fi
+
+	# download CNS11643中文標準交換碼全字庫（簡稱全字庫）
+	# 正楷體、正宋體
+	# TW-Kai, TW-Sung
+	wget https://www.cns11643.gov.tw/opendata/Fonts_Kai.zip
+fi
+
+if [ -f "Fonts_Kai.zip" ] ; then
+    # let's hash it~
+    # but we don't have offical sha1sum file Orz
+    hash=$(sha1sum Fonts_Kai.zip | cut -d ' ' -f 1)
+    echo -e "\n The SHA1 value of Fonts_Kai.zip is \n"
+    echo -e "\t>>>>> $hash <<<<<\n"
+
+    # unzip
+    unzip Fonts_Kai.zip -d Open_Data
+fi
+
+# ---------------------------
+# 安裝
+# ---------------------------
 
 # check zip file download and upzip succefully
 if [ ! -d "Open_Data/" ] ; then
@@ -66,13 +173,18 @@ if [ ! -d "Open_Data/" ] ; then
 fi
 
 # copy fonts to ~/.fonts
-cp -i Open_Data/Fonts/*.ttf $HOME/.fonts
+if [ -d "Open_data/Fonts/" ] ; then
+    cp -i Open_Data/Fonts/*.ttf $HOME/.fonts
+fi
+cp -i Open_Data/*.ttf $HOME/.fonts
 
 # refresh fonts cache
 sudo fc-cache -fv
 
 # remove zip and unzip dir
 rm Open_Data.zip
+rm Fonts_Sung.zip
+rm Fonts_Kai.zip
 rm -r Open_Data/
 
 # finally
